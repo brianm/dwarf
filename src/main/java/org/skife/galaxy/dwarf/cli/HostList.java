b39019546f1b0b534c3d17aea191bcd8402a1ef2
Brian McCallister <brianm@skife.org>
2012-07-22 23:41:52 -0600

    nice output for host list

--- src/main/java/org/skife/galaxy/dwarf/cli/HostList.java
+++ src/main/java/org/skife/galaxy/dwarf/cli/HostList.java
@@ -2,13 +2,17 @@ package org.skife.galaxy.dwarf.cli;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import org.skife.cli.Command;
 import org.skife.galaxy.dwarf.Dwarf;
 import org.skife.galaxy.dwarf.Host;
+import org.skife.galaxy.dwarf.cli.util.TabularRenderer;
 import org.skife.galaxy.dwarf.state.file.FileState;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 @Command(name={"list", "ls"})
@@ -22,9 +26,11 @@ public class HostList implements Callable<Void>
         FileState state = new FileState(Paths.get(".dwarf"));
         Dwarf d = new Dwarf(state, "/tmp/dwarf", Optional.<Path>absent());
 
+        List<List<String>> table = Lists.newArrayList();
         for (Host host : d.getHosts()) {
-            System.out.println(mapper.writeValueAsString(host));
+            table.add(ImmutableList.of(host.getHostname()));
         }
+        TabularRenderer.render(table);
         return null;
     }
 }
